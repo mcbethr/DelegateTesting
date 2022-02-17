172d35348f3035f7f733b77766d124b0ecb3025d
Ryan <ryan@ryanmcbeth.com>
2022-02-17 11:29:46 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DelegateTesting{
+
+class Program{
+
+    static void Main()
+    {
+        Examples Ex = new Examples();
+
+    Console.WriteLine(Ex.ReadOnlyNumber);
+
+        Examples EX2 = new Examples(2);
+        Console.WriteLine(EX2.ReadOnlyNumber);
+
+    }
+
+}
+
+
+}
